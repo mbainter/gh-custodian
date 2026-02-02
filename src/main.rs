09d304f857617b29390b3adeab8c5be69c6b567e
mbainter <mbainter+github@gmail.com>
2026-02-01 20:14:27 -0600

    feat: initial basic structure

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use anyhow::{bail, Ok, Result};
+use std::env;
+use std::path::PathBuf;
+use clap::Parser;
+use log::{debug};
+use octorust::{auth::{Credentials}, Client};
+
+/// GitHub policy-based management for enterprises and organizations
+/// https://github.com/mbainter/gh-custodian
+#[derive(Parser, Debug)]
+struct Args {
+    /// The path to the policy file
+    policy: Option<PathBuf>,
+
+    #[command(flatten)]
+    verbosity: clap_verbosity_flag::Verbosity,
+}
+
+fn create_client() -> Result<Client> {
+    let token = env::var("GITHUB_TOKEN");
+
+    if token.is_err() {
+        bail!("GITHUB_TOKEN must be set");
+    }
+
+    let github = Client::new(
+        "gh-custodian",
+        Credentials::Token(token.unwrap())
+    )?;
+
+    Ok(github)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    env_logger::Builder::new()
+        .filter_level(args.verbosity.into())
+        .init();
+
+    debug!("policy: {:?}", args.policy);
+
+    let github = create_client()?;
+
+    let repo = github
+        .repos()
+        .get("mbainter", "gh-custodian")
+        .await
+        .unwrap();
+
+    dbg!(repo);
+
+    Ok(())
+}
---
+++ tests/init.rs
@@ -0,0 +1,15 @@
+use std::env;
+use assert_cmd::cargo::*;
+use predicates::prelude::*;
+
+#[test]
+fn github_token_missing() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = cargo_bin_cmd!("gh-custodian");
+
+    cmd.env_remove("GITHUB_TOKEN");
+    cmd.assert()
+      .failure()
+      .stderr(predicate::str::contains("GITHUB_TOKEN must be set"));
+
+    Ok(())
+}
