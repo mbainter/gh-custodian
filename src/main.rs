6ba5ebc4b223eb5d834037fe0d7223dc09054d58
mbainter <mbainter+github@gmail.com>
2026-03-14 16:46:41 -0500

    feat: add parsing for a basic policy check

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,42 @@ use anyhow::{Ok, Result, bail};
 use clap::Parser;
 use log::debug;
 use octorust::{Client, auth::Credentials};
+use serde::Deserialize;
 use std::env;
+use std::fs;
 use std::path::PathBuf;
+
+#[derive(Debug, Deserialize)]
+struct Policy {
+    organization: String,
+    repository_targets: Vec<String>,
+    allowed_repo_roles: Vec<String>,
+    authorized_individuals: Vec<IndividualAccessPolicy>,
+    authorized_repo_admins: Vec<AuthorizationPolicy>,
+    authorized_repo_maintainers: Vec<AuthorizationPolicy>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+enum IndividualAccessPolicy {
+    Role { name: String },
+}
+
+#[derive(Debug, Deserialize)]
+enum TargetType {
+    User,
+    Team,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+enum AuthorizationPolicy {
+    CustomProperty {
+        property: String,
+        target_type: TargetType,
+        role: String,
+    },
+}
 
 /// GitHub policy-based management for enterprises and organizations
 /// https://github.com/mbainter/gh-custodian
@@ -34,7 +68,12 @@ async fn main() -> Result<()> {
     env_logger::Builder::new().filter_level(args.verbosity.into()).init();
 
     debug!("policy: {:?}", args.policy);
+    let policy_data = fs::read_to_string(args.policy.unwrap()).expect("Error reading policy file");
+    let policy: Policy = toml::from_str(&policy_data)?;
+    debug!("policy: {:?}", policy);
+    println!("org: {}", policy.organization);
 
+    /*
     let gh = create_client().unwrap();
 
     let repo = gh.repos().get("mbainter", "gh-custodian").await?;
@@ -44,6 +83,7 @@ async fn main() -> Result<()> {
     let rulesets = gh.repos().get_org_rulesets("litmus", 10, 1, "").await?;
 
     dbg!(rulesets);
+    */
 
     Ok(())
 }
